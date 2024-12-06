3db52971c6d8289d7b45d019d95db8a6172d3338
VenganzaJ <bbani1054@gmail.com>
2024-12-06 17:43:42 -0600

    Login update
    Contiene lo necesario para el login aunque, realmente no esta conectado aun

---
+++ main_login.py
@@ -0,0 +1,30 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from Ventana_Login import Ui_MainWindow as Ui_login
+
+class LoginWindow(QMainWindow, Ui_login):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        
+        
+        self.pushButton.clicked.connect(self.LOGIN)
+        self.pushButton_Salir.clicked.connect(self.salir)
+        
+        self.Historial_mostrar = None
+
+    def LOGIN(self):
+        username = self.user_edit.text()  
+        password = self.paswr_edit.text()  
+        
+        if username == "admin" and password == "123456":
+            self.close()  
+
+    def salir(self):
+        sys.exit() 
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = LoginWindow()
+    window.show()
+    sys.exit(app.exec())
