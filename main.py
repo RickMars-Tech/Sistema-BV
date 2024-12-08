7a257e705f56e4127b35cf7d5ee2f4c81306afaa
RickMars <139187404+RickMars-Tech@users.noreply.github.com>
2024-12-07 23:52:37 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -25,24 +25,24 @@ class Ventana(QMainWindow, Ui_MainWindow):
         #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~@
         self.agr_lib_btm.clicked.connect(self.agregar_libro)
         self.consultar_libro_btm.clicked.connect(self.consultar_libro)
-        self.cancel_agr_lib_btm.clicked.connect(self.cancelar_agregar_libro)
+        #self.cancel_agr_lib_btm.clicked.connect(self.cancelar_agregar_libro)
         self.edit_lib_btm.clicked.connect(self.editar_libro)
-        self.cancel_edit_lib_btm.clicked.connect(self.cancelar_editar_libro)
+        #self.cancel_edit_lib_btm.clicked.connect(self.cancelar_editar_libro)
         #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~@
         # Acciones de Botones en Tab de Usuarios
         #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~@
         self.reg_usr_btm.clicked.connect(self.registrar_usuario)
         self.consultar_usuarios_btm.clicked.connect(self.consultar_usuarios)
-        self.cancel_reg_usr_btm.clicked.connect(self.cancelar_registro_usuario)
+        #self.cancel_reg_usr_btm.clicked.connect(self.cancelar_registro_usuario)
         self.edit_usr_btm.clicked.connect(self.editar_usuario)
-        self.cancel_edit_usr_btm.clicked.connect(self.cancelar_editar_usuario)
+        #self.cancel_edit_usr_btm.clicked.connect(self.cancelar_editar_usuario)
         #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~@
         # Acciones de Botones en Tab de Prestamos
         #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~@
         self.reg_prestamo_btm.clicked.connect(self.registrar_prestamo)
-        self.cancel_reg_prestamo_btm.clicked.connect(self.cancelar_prestamo)
+        #self.cancel_reg_prestamo_btm.clicked.connect(self.cancelar_prestamo)
         self.reg_devolucion_btm.clicked.connect(self.registrar_devolucion)
-        self.cancel_reg_devolucion_btm.clicked.connect(self.cancelar_devolucion)
+        #self.cancel_reg_devolucion_btm.clicked.connect(self.cancelar_devolucion)
         #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~@
         # Acciones de Botones en Tab de Historial
         #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~@
@@ -84,11 +84,6 @@ class Ventana(QMainWindow, Ui_MainWindow):
         for libro in libros:
             self.list_libros.addItem(libro)  # Añadir cada libro a la lista
 
-    def cancelar_agregar_libro(self):
-        self.Ag_LibTitulo_LEdit.clear()
-        self.Ag_LibAutor_LEdit.clear()
-        self.Ag_LibISBN_LEdit.clear()
-
     def editar_libro(self):
         isbn = self.selct_isbn_comboBx.currentText()
         titulo = self.edit_LibTitulo_LEdit.text()
@@ -109,11 +104,6 @@ class Ventana(QMainWindow, Ui_MainWindow):
         self.edit_LibEstado_LEdit.clear()
         self.consultar_libro()  # Actualizar la lista de libros
 
-    def cancelar_editar_libro(self):
-        self.edit_LibTitulo_LEdit.clear()
-        self.edit_LibAutor_LEdit.clear()
-        self.edit_LibEstado_LEdit.clear()
-
     # Funciones para Gestión de Usuarios
     def registrar_usuario(self):
         id_usuario = self.Reg_UsrID_LEdit.text()
@@ -132,11 +122,6 @@ class Ventana(QMainWindow, Ui_MainWindow):
         for usuario in usuarios:
             self.listUsuarios.addItem(usuario)  # Añadir cada usuario a la lista
 
-    def cancelar_registro_usuario(self):
-        self.Reg_UsrID_LEdit.clear()
-        self.Reg_UsrName_LEdit.clear()
-        self.Reg_UsrRol_LEdit.clear()
-
     def editar_usuario(self):
         id_usuario = self.selct_usrID_comboBx.currentText()
         nombre = self.Edit_Reg_UsrName_LEdit.text()
@@ -154,10 +139,6 @@ class Ventana(QMainWindow, Ui_MainWindow):
         self.Edit_Reg_UsrName_LEdit.clear()
         self.Edit_Reg_UsrRol_LEdit.clear()
         self.consultar_usuarios()  # Actualizar la lista de usuarios
-
-    def cancelar_editar_usuario(self):
-        self.Edit_Reg_UsrName_LEdit.clear()
-        self.Edit_Reg_UsrRol_LEdit.clear()
 
     # Funciones para Gestión de Préstamos
     def registrar_prestamo(self):
@@ -202,12 +183,6 @@ class Ventana(QMainWindow, Ui_MainWindow):
             archivo.write('\n'.join(nuevo_estado_libros) + '\n')
         self.consultar_libro()
         self.consultar_historial()
-
-    def cancelar_prestamo(self):
-        pass
-
-    def cancelar_devolucion(self):
-        pass
 
     # Función para consultar historial
     def consultar_historial(self):
